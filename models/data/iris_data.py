2a880ee39e024fd7c25d4c6ed5b579b78425f2e4
Peng-jancy <1611304708@qq.com>
2025-09-28 20:35:05 +0800

    Add files via upload

---
+++ models/data/iris_data.py
@@ -0,0 +1,26 @@
+# data/iris_data.py
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from config import batch_size
+
+def load_data(test_size=0.2):
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    
+    # 转成 tensor
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.long)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.long)
+    
+    train_dataset = TensorDataset(X_train, y_train)
+    test_dataset = TensorDataset(X_test, y_test)
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_loader, test_loader
